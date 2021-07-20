0fc0a1929f98748502736c51a995b88f02a281f2
47-hlj <hlj172532>
2021-07-20 15:43:48 +0800

    测试ribbon重试

--- springcloud1/sp02-itemservice/src/main/java/cn/tedu/sp02/item/controller/ItemController.java
+++ springcloud1/sp02-itemservice/src/main/java/cn/tedu/sp02/item/controller/ItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * @author 47HLJ
@@ -27,7 +28,7 @@ public class ItemController {
      * @return
      */
     @GetMapping("/{orderId}")
-    public JsonResult<List<Item>> getItems(@PathVariable String orderId){
+    public JsonResult<List<Item>> getItems(@PathVariable String orderId) throws InterruptedException {
         /*
         JsonResult - 响应结构的封装对象
                    - code：响应的状态码，可以是http状态，或者也可以自己任意定义
@@ -35,6 +36,16 @@ public class ItemController {
                    - data：发回的响应数据对象
          */
         List<Item> items=itemService.getItems(orderId);
+        // 为了测试响应超时失败，添加随机延迟代码
+        double s=0.9;
+        if(Math.random()<s){
+            //90%概率延迟加载
+            //随机时长0到5秒
+            int t = new Random().nextInt(5000);
+            System.out.println("延迟： "+t);
+            Thread.sleep(t);
+
+        }
         return JsonResult.ok().data(items);
     }
 
@@ -51,6 +62,5 @@ public class ItemController {
         itemService.decreaseNumber(items);
         return JsonResult.ok().msg("减少商品库存成功");
     }
-
 
 }
