8c13cb67f526cf9ac726e29ecc9af63bc5ebb26c
47-hlj <hlj172532>
2021-07-28 16:28:27 +0800

    eureka server

---
+++ seata-at/eureka-server/src/main/java/cn/tedu/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package cn.tedu.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 47HLJ
+ * @date 2021/7/28 15:25
+ * @description
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
